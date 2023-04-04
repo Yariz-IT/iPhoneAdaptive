0f9b646a5862101fa9dfedd3a0f38b4c1db6a708
Yariz-IT <116900067+Yariz-IT@users.noreply.github.com>
2023-04-04 11:50:40 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+const btnNew= document.querySelector('.new-btn-link');
+const newBlock = document.querySelector('.new');
+const btnColor = document.querySelector('.btn-color');
+const colorBlock = document.querySelector('.color');
+const btnBuy = document.querySelector('.btn-buy');
+const contactsBlock = document.querySelector('.contacts');
+
+function handleButtonClick(e) {
+    const arrBlock =[newBlock, colorBlock, contactsBlock]
+   const id =e.target.id
+    arrBlock[id].scrollIntoView({block: "center", behavior: "smooth"});
+ }
+
+ btnNew.addEventListener('click', handleButtonClick);
+ btnColor.addEventListener('click', handleButtonClick);
+ btnBuy.addEventListener('click', handleButtonClick);
+
+
+ 
+btn.addEventListener('click', ()=>{
+   nav.classList.toggle('menu-open');
+})
+
+
